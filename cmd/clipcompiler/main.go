d903e96bec580dd53c55f3d52c623ac00c763e7e
jaaanko <janko2454@gmail.com>
2023-10-12 01:14:28 +0800

    Automatically download clips from links

--- cmd/clipcompiler/main.go
+++ cmd/clipcompiler/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
+	"sync"
 
 	"github.com/jaaanko/twitch-clip-compilation-tool/internal/twitch"
 	"github.com/joho/godotenv"
@@ -43,10 +46,47 @@ func main() {
 	}
 
 	clipLinks, err := twitchSvc.GetClipURLs(broadcasterId, startDate, count)
-
+	// clipLinks, err := twitchSvc.GetClipURLs("0", "2023-10-01T00:00:00Z", 10)
 	if err != nil {
 		log.Fatal("Cannot fetch clips")
 	}
 
 	fmt.Println(clipLinks)
+	var wg sync.WaitGroup
+
+	for i, link := range clipLinks {
+		path := fmt.Sprintf("clip%d.mp4", i)
+		wg.Add(1)
+		go func(path, link string) {
+			defer wg.Done()
+			saveClip(path, link)
+		}(path, link)
+	}
+
+	wg.Wait()
+}
+
+func saveClip(path, link string) {
+	res, err := http.Get(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatal("expected 200, got: ", res.Status)
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		log.Fatal("failed to create file")
+	}
+
+	defer file.Close()
+
+	_, err = io.Copy(file, res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
