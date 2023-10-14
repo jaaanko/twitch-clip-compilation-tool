8ede3ad5b6041c11aac2545e261ab21b51172f48
jaaanko <janko2454@gmail.com>
2023-10-14 21:20:53 +0800

    Add tests for twitch package

---
+++ internal/twitch/twitch_test.go
@@ -0,0 +1,86 @@
+package twitch_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/jaaanko/twitch-clip-compilation-tool/internal/twitch"
+)
+
+func TestGetClipURLs_DoesNotReturnStatusOK(t *testing.T) {
+	authSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{
+			"access_token": "testtoken123",
+			"expires_in": 5513382,
+			"token_type": "bearer"
+		}`))
+	}))
+	defer authSvr.Close()
+
+	apiSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusBadRequest)
+	}))
+	defer apiSvr.Close()
+
+	twitchSvc, err := twitch.NewService("client_id", "client_secret", authSvr.URL, apiSvr.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = twitchSvc.GetClipURLs("0", "2023-10-05T00:00:00Z", 10)
+	if err == nil {
+		t.Error("expected an error")
+	}
+}
+
+func TestGetClipURLs_ReturnsStatusOK(t *testing.T) {
+	authSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{
+			"access_token": "testtoken123",
+			"expires_in": 5513382,
+			"token_type": "bearer"
+		}`))
+
+	}))
+	defer authSvr.Close()
+
+	testClipID := "testClipID1"
+	testURL := fmt.Sprintf("https://clips.twitch.tv/%s", testClipID)
+	testDuration := 25
+	testThumbnailURL := "https://clips-media-assets2.twitch.tv/test1/12345-offset-20320-preview-480x272.jpg"
+
+	apiSvr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fmt.Sprintf(`{
+			"data": [
+				{
+					"id": "%s",
+					"url": "%s",
+					"duration": %d,
+					"thumbnail_url": "%s"
+				}
+			]
+		}`, testClipID, testURL, testDuration, testThumbnailURL)))
+	}))
+	defer apiSvr.Close()
+
+	twitchSvc, err := twitch.NewService("client_id", "client_secret", authSvr.URL, apiSvr.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	clips, err := twitchSvc.GetClipURLs("0", "2023-10-05T00:00:00Z", 10)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(clips) == 0 {
+		t.Error("no clips returned")
+	}
+
+	expected := "https://clips-media-assets2.twitch.tv/test1/12345-offset-20320.mp4"
+	if clips[0] != expected {
+		t.Errorf("expected: %s, got: %s", expected, clips[0])
+	}
+}
