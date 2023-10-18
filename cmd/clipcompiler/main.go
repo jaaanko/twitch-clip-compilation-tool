b25e5c01360913533103db650e8d671baf79c477
jaaanko <janko2454@gmail.com>
2023-10-18 23:16:02 +0800

    Prepare for concatenation after downloading clips

--- cmd/clipcompiler/main.go
+++ cmd/clipcompiler/main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 	"sync"
 
 	"github.com/jaaanko/twitch-clip-compilation-tool/internal/twitch"
 	"github.com/joho/godotenv"
 )
+
+const outputDir = "out"
 
 func main() {
 	err := godotenv.Load()
@@ -46,47 +50,81 @@ func main() {
 	}
 
 	clipLinks, err := twitchSvc.GetClipURLs(broadcasterId, startDate, count)
-	// clipLinks, err := twitchSvc.GetClipURLs("0", "2023-10-01T00:00:00Z", 10)
 	if err != nil {
 		log.Fatal("Cannot fetch clips")
 	}
 
 	fmt.Println(clipLinks)
 	var wg sync.WaitGroup
+	var paths []string
+
+	err = os.MkdirAll(outputDir, 0750)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for i, link := range clipLinks {
 		path := fmt.Sprintf("clip%d.mp4", i)
 		wg.Add(1)
 		go func(path, link string) {
 			defer wg.Done()
+			// TODO: better error handling
 			saveClip(path, link)
+			paths = append(paths, path)
 		}(path, link)
 	}
 
 	wg.Wait()
+
+	file, err := os.Create(filepath.Join(outputDir, "list.txt"))
+	if err != nil {
+		log.Fatalf("failed to create file: %v", err)
+	}
+
+	defer file.Close()
+	err = writeFileNames(paths, file)
+	if err != nil {
+		log.Println(err)
+	}
+
 }
 
 func saveClip(path, link string) {
 	res, err := http.Get(link)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		log.Fatal("expected 200, got: ", res.Status)
+		log.Printf("expected 200, got: %v", res.Status)
+		return
 	}
 
-	file, err := os.Create(path)
+	file, err := os.Create(filepath.Join(outputDir, path))
 	if err != nil {
-		log.Fatal("failed to create file")
+		log.Printf("failed to create file: %v", err)
+		return
 	}
 
 	defer file.Close()
 
 	_, err = io.Copy(file, res.Body)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
+}
+
+func writeFileNames(fileNames []string, dest io.Writer) error {
+	var errFailedToWrite error
+	for _, fileName := range fileNames {
+		_, err := dest.Write([]byte(fmt.Sprintf("file '%v'\n", fileName)))
+		if err != nil {
+			errFailedToWrite = errors.Join(errFailedToWrite, err)
+		}
+	}
+	return errFailedToWrite
 }
