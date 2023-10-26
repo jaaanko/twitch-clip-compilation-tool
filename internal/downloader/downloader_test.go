0e402608217a91ac135fbfb415155b7c5572d823
jaaanko <janko2454@gmail.com>
2023-10-27 01:54:52 +0800

    Refactor downloader tests to use test tables

--- internal/downloader/downloader_test.go
+++ internal/downloader/downloader_test.go
@@ -5,15 +5,21 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/jaaanko/twitch-clip-compilation-tool/internal/downloader"
 )
 
-func TestRun_ReturnsErrorWhenDownloadPartiallyFails(t *testing.T) {
+type result struct {
+	numDownloaded int
+	hasError      bool
+}
+
+func TestRun(t *testing.T) {
 	clip1 := "/example1.mp4"
 	clip2 := "/example2.mp4"
-	clipServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	partialFailServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == clip1 {
 			w.WriteHeader(http.StatusInternalServerError)
 		} else {
@@ -21,83 +27,47 @@ func TestRun_ReturnsErrorWhenDownloadPartiallyFails(t *testing.T) {
 		}
 	}))
 
-	clipURL1, _ := url.JoinPath(clipServer.URL, clip1)
-	clipURL2, _ := url.JoinPath(clipServer.URL, clip2)
-	urls := []string{
-		clipURL1,
-		clipURL2,
-	}
-
-	tempDir := t.TempDir()
-	if err := downloader.Run(tempDir, urls); err == nil {
-		t.Fatalf("expected an error")
-	}
-
-	numFiles, err := numberOfFiles(tempDir)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if numFiles != 1 {
-		t.Errorf("expected one file to be downloaded, got %v", numFiles)
-	}
-}
-
-func TestRun_ReturnsErrorWhenDownloadFullyFails(t *testing.T) {
-	clip1 := "/example1.mp4"
-	clip2 := "/example2.mp4"
-	clipServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	fullFailServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 	}))
 
-	clipURL1, _ := url.JoinPath(clipServer.URL, clip1)
-	clipURL2, _ := url.JoinPath(clipServer.URL, clip2)
-	urls := []string{
-		clipURL1,
-		clipURL2,
-	}
-
-	tempDir := t.TempDir()
-	if err := downloader.Run(tempDir, urls); err == nil {
-		t.Fatalf("expected an error")
-	}
-
-	numFiles, err := numberOfFiles(tempDir)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if numFiles != 0 {
-		t.Errorf("expected 0 files to be downloaded, got %v", numFiles)
-	}
-}
-
-func TestRun_ReturnsNoErrorWhenDownloadSucceeds(t *testing.T) {
-	clip1 := "/example1.mp4"
-	clip2 := "/example2.mp4"
-	clipServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	successServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("clip data"))
 	}))
 
-	clipURL1, _ := url.JoinPath(clipServer.URL, clip1)
-	clipURL2, _ := url.JoinPath(clipServer.URL, clip2)
-	urls := []string{
-		clipURL1,
-		clipURL2,
+	tests := map[string]struct {
+		server *httptest.Server
+		want   result
+	}{
+		"download partial fail": {server: partialFailServer, want: result{numDownloaded: 1, hasError: true}},
+		"download full fail":    {server: fullFailServer, want: result{numDownloaded: 0, hasError: true}},
+		"download success":      {server: successServer, want: result{numDownloaded: 2, hasError: false}},
 	}
 
-	tempDir := t.TempDir()
-	if err := downloader.Run(tempDir, urls); err != nil {
-		t.Fatalf("expected no errors")
-	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			clipURL1, _ := url.JoinPath(tc.server.URL, clip1)
+			clipURL2, _ := url.JoinPath(tc.server.URL, clip2)
+			urls := []string{clipURL1, clipURL2}
+			tempDir := t.TempDir()
 
-	numFiles, err := numberOfFiles(tempDir)
-	if err != nil {
-		t.Error(err)
-	}
+			errDownload := downloader.Run(tempDir, urls)
+			hasError := errDownload != nil
 
-	if numFiles != 2 {
-		t.Errorf("expected 2 files to be downloaded, got %v", numFiles)
+			numFiles, err := numberOfFiles(tempDir)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			got := result{numDownloaded: numFiles, hasError: hasError}
+			if !reflect.DeepEqual(tc.want, got) {
+				if tc.want.hasError != got.hasError {
+					t.Fatalf("expected: %#v, got: %#v, error: %v", tc.want, got, errDownload)
+				} else {
+					t.Fatalf("expected: %#v, got: %#v", tc.want, got)
+				}
+			}
+		})
 	}
 }
 
